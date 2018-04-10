77ff9bebaa785b29ee4a4eccbfbe6fb071dc8a8f
WangGY <11510050@mail.sustc.edu.cn>
2018-04-10 21:52:18 +0800

    Build Server

---
+++ Server/src/main/java/util/WeijuApp.java
@@ -0,0 +1,13 @@
+package util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeijuApp {
+    public static void main(String[] args){
+        SpringApplication.run(WeijuApp.class,args);
+
+    }
+
+}
